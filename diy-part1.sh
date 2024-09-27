71aae5fcbbf15ef2482ace5936142a5abbe47688
aaa7260 <62923926+aaa7260@users.noreply.github.com>
2024-09-28 00:09:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,6 @@
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo "src-git cups https://github.com/Gr4ffy/lede-cups.git" >> feeds.conf.default
+#echo "src-git cups https://github.com/Gr4ffy/lede-cups.git" >> feeds.conf.default
+echo "src-git cups https://github.com/SoPudge/lede-cups" >> feeds.conf.default
 
