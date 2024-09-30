b8641296421497db9074a7017ff983f73b01d2fe
aaa7260 <62923926+aaa7260@users.noreply.github.com>
2024-09-30 11:54:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,14 +12,20 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-#sed -i 's/PKG_VERSION:=2.1.4/PKG_VERSION:=2.3.3/g' package/feeds/cups/cups/Makefile
-#sed -i '/^PKG_SOURCE_UR/d' package/feeds/cups/cups/Makefile
-#sed -i '/^PKG_MD5SUM/d' package/feeds/cups/cups/Makefile
+#sed -i 's/PKG_VERSION:=2.1.4/PKG_VERSION:=2.3.3/g' feeds/cups/cups/Makefile
+#sed -i '/^PKG_SOURCE_UR/d' feeds/cups/cups/Makefile
+#sed -i '/^PKG_MD5SUM/d' feeds/cups/cups/Makefile
 sed -i '/^PKG_SOURCE_UR/d' feeds/cups/cups/Makefile
 
 # cp -f $GITHUB_WORKSPACE/cups-2.3.3-source.tar.gz dl/cups-2.3.3-source.tar.gz
-#rm -rf package/feeds/cups/cups/patch
-#sed -i 's/\/$(PKG_VERSION)/\/release-$(PKG_VERSION)\/$(PKG_SOURCE)/g' package/feeds/cups/cups/Makefile
+#rm -rf feeds/cups/cups/patch
+#sed -i 's/\/$(PKG_VERSION)/\/release-$(PKG_VERSION)\/$(PKG_SOURCE)/g' feeds/cups/cups/Makefile
 #https://github.com/apple/cups/releases/download/v2.3.3/cups-2.3.3-source.tar.gz
 #https://github.com/apple/cups/releases/download/release-2.1.4/cups-2.1.4-source.tar.gz
-#sed -i 's/\/$(PKG_VERSION)/\/release-2.1.4/g' package/feeds/cups/cups/Makefile
+#sed -i 's/\/$(PKG_VERSION)/\/release-2.1.4/g' feeds/cups/cups/Makefile
+
+
+sed -i 's/PKG_VERSION:=22.01.0/PKG_VERSION:=24.09.0/g' feeds/cups/poppler/Makefile
+sed -i 's/gz/xz/g' feeds/cups/poppler/Makefile
+#ed -i '##^PKG_SOURCE_UR#PKG_SOURCE_URL:=https://poppler.freedesktop.org#g' feeds/cups/poppler/Makefile
+sed -i 's#/^PKG_MD5SUM#KG_MD5SUM:=CA262C6C83DE4D2FB28A332A3A6A4DB839EAA6D7#g' feeds/cups/poppler/Makefile
